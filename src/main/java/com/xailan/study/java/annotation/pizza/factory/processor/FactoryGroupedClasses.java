0d81a73d1066ab38af4e682af8c9ddb07331fadb
sealang <xailan2017@hotmail.com>
2017-08-24 23:07:22 +0800

    新增了方法

--- src/main/java/com/xailan/study/java/annotation/pizza/factory/processor/FactoryGroupedClasses.java
+++ src/main/java/com/xailan/study/java/annotation/pizza/factory/processor/FactoryGroupedClasses.java
@@ -1,15 +1,20 @@
 package com.xailan.study.java.annotation.pizza.factory.processor;
 
 import java.io.IOException;
+import java.io.Writer;
 import java.util.LinkedHashMap;
 import java.util.Map;
 
 import javax.annotation.processing.Filer;
+import javax.lang.model.element.TypeElement;
 import javax.lang.model.util.Elements;
+import javax.tools.JavaFileObject;
 
 import com.xailan.study.java.annotation.pizza.factory.Factory;
 
 public class FactoryGroupedClasses {
+	private static final String SUFFIX = "Factory";
+
 	private String qualifiedClassName;
 	private Map<String, FactoryAnnotatedClass> itemsMap = new LinkedHashMap<String, FactoryAnnotatedClass>();
 
@@ -31,6 +36,38 @@ public class FactoryGroupedClasses {
 	}
 
 	public void generateCode(Elements elementUtils, Filer filer) throws IOException {
-		
+		TypeElement superClassName = elementUtils.getTypeElement(qualifiedClassName);
+		String factoryClassName = superClassName.getSimpleName() + SUFFIX;
+		JavaFileObject jfo = filer.createSourceFile(qualifiedClassName + SUFFIX);
+		Writer writer = jfo.openWriter();
+		JavaWriter jw = new JavaWriter(writer);
+		// 写包名
+		PackageElement pkg = elementUtils.getPackageOf(superClassName);
+		if (!pkg.isUnnamed()) {
+			jw.emitPackage(pkg.getQualifiedName().toString());
+			jw.emitEmptyLine();
+		} else {
+			jw.emitPackage("");
+		}
+
+		jw.beginType(factoryClassName, "class", EnumSet.of(Modifier.PUBLIC));
+		jw.emitEmptyLine();
+		jw.beginMethod(qualifiedClassName, "create", EnumSet.of(Modifier.PUBLIC), "String", "id");
+
+		jw.beginControlFlow("if (id == null)");
+		jw.emitStatement("throw new IllegalArgumentException(\"id is null!\")");
+		jw.endControlFlow();
+
+		for (FactoryAnnotatedClass item : itemsMap.values()) {
+			jw.beginControlFlow("if (\"%s\".equals(id))", item.getId());
+			jw.emitStatement("return new %s()", item.getTypeElement().getQualifiedName().toString());
+			jw.endControlFlow();
+			jw.emitEmptyLine();
+		}
+
+		jw.emitStatement("throw new IllegalArgumentException(\"Unknown id = \" + id)");
+		jw.endMethod();
+		jw.endType();
+		jw.close();
 	}
 }
